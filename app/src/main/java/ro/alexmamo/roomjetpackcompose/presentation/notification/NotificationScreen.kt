152d21ae8d5e6f7b1f76f232d533f185ab1c20f6
Christian <b.christian.fuentes@gmail.com>
2025-11-01 00:54:56 -0300

    modificaciones

--- app/src/main/java/ro/alexmamo/roomjetpackcompose/presentation/notification/NotificationScreen.kt
+++ app/src/main/java/ro/alexmamo/roomjetpackcompose/presentation/notification/NotificationScreen.kt
@@ -10,28 +10,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ro.alexmamo.roomjetpackcompose.R
+import ro.alexmamo.roomjetpackcompose.components.ActionIconButton
 import ro.alexmamo.roomjetpackcompose.components.AppTopBar
 import ro.alexmamo.roomjetpackcompose.components.BottomNavigationBar
 import ro.alexmamo.roomjetpackcompose.components.NotificationItem
 import ro.alexmamo.roomjetpackcompose.presentation.layouts.BaseScreen
-import ro.alexmamo.roomjetpackcompose.ui.theme.CaribbeanGreen
 import ro.alexmamo.roomjetpackcompose.ui.theme.Dimens
+import ro.alexmamo.roomjetpackcompose.ui.theme.Honeydew
 
 @Composable
 fun NotificationScreen(vm: NotificationViewModel = viewModel()) {
@@ -41,25 +37,34 @@ fun NotificationScreen(vm: NotificationViewModel = viewModel()) {
             AppTopBar(
                 title = stringResource(R.string.notification_title),
                 leftAction = {
-                    IconButton(onClick = { /* TODO: navigate back */ }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.arrow_left),
-                            contentDescription = "Back"
-                        )
-                    }
+                    ActionIconButton(
+                        onActionIconButtonClick = { /* para atras */ },
+                        withCircle = false,
+                        content = { mod ->
+                            androidx.compose.material.Icon(
+                                painter = painterResource(id = R.drawable.arrow_left),
+                                contentDescription = stringResource(id = R.string.navigate_back),
+                                tint = Honeydew,
+                                modifier = mod
+                            )
+                        }
+                    )
                 },
                 rightAction = {
-
-                    IconButton(onClick = { /* TODO: open notifications/settings */ }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.notification),
-                            contentDescription = "Notifications",
-                            modifier = Modifier
-                                .clip(CircleShape)
-                                .background(MaterialTheme.colorScheme.surface),
-                            tint = MaterialTheme.colorScheme.onSecondary
-                        )
-                    }
+                    ActionIconButton(
+                        onActionIconButtonClick = { /* esto la verdad nose qe haria porque ya estamos en notif */ },
+                        withCircle = true,
+                        circleSize = 30.dp,
+                        circleColor = MaterialTheme.colorScheme.surface,
+                        content = { mod ->
+                            androidx.compose.material.Icon(
+                                painter = painterResource(id = R.drawable.notification),
+                                contentDescription = stringResource(id = R.string.notification_title),
+                                tint = MaterialTheme.colorScheme.onSecondary,
+                                modifier = mod
+                            )
+                        }
+                    )
                 }
             )
         },
@@ -90,7 +95,7 @@ fun NotificationScreen(vm: NotificationViewModel = viewModel()) {
                             messageRes = item.messageRes,
                             timeRes = item.timeRes
                         )
-                        // Divider con color de background del tema y grosor 2.dp
+
                         Box(
                             modifier = Modifier
                                 .fillMaxWidth()
