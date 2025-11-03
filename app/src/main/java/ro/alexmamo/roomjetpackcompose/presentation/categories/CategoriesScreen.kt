29bbfcc4480da5a3e6b8320be15fec368c4356aa
luisacosta121 <luisacosta121@gmail.com>
2025-11-03 08:18:01 -0300

    Se modifica pantalla de categorias

--- app/src/main/java/ro/alexmamo/roomjetpackcompose/presentation/categories/CategoriesScreen.kt
+++ app/src/main/java/ro/alexmamo/roomjetpackcompose/presentation/categories/CategoriesScreen.kt
@@ -2,40 +2,26 @@ package ro.alexmamo.roomjetpackcompose.presentation.categories
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import ro.alexmamo.roomjetpackcompose.R
 import ro.alexmamo.roomjetpackcompose.components.BottomNavigationBar
+import ro.alexmamo.roomjetpackcompose.presentation.categories.components.GridCategories
 import ro.alexmamo.roomjetpackcompose.presentation.layouts.BaseScreen
-import ro.alexmamo.roomjetpackcompose.presentation.profile.UserViewModel
 
 @Composable
 fun CategoriesScreen(
     navController: NavHostController,
-    viewModel: UserViewModel = viewModel()
-) {
-    val uiState by viewModel.uiState.collectAsState()
 
-    LaunchedEffect(Unit) {
-        viewModel.fetchUser(1)
-    }
-
+    ) {
     BaseScreen(
-        title = "Categories",
+        title = stringResource(R.string.categories),
         bottomBar = { BottomNavigationBar(navController) },
         content = {
             Column(
@@ -45,7 +31,7 @@ fun CategoriesScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-
+                GridCategories()
             }
         }
     )
